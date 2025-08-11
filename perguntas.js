3cf489cb760f65977aa1fa327372563dd4940035
Wesleywk123 <125040704+wesleywk123@users.noreply.github.com>
2025-08-11 08:52:14 -0300

    l

--- perguntas.js
+++ perguntas.js
@@ -1,18 +1,34 @@
 criaCartao(
-    Geografia,
-    Qual a capital da França,
-    Paris
+    'Geografia',
+    'Qual a capital da França?',
+    'Paris'
 )
 
 criaCartao(
-    Matemática,
-    13^2,
-    169
+    'Matemática',
+    '13^2',
+    '169'
 )
 
 criaCartao(
-    Carros,
-    Diferença entre V12 e 12V,
-    V12 corresponde ao formato do e quantidade cilindros do motor, já 12v, revela a quantidade de válvulas no motor "12 válvulas".
+    'Carros',
+    'Diferença entre V12 e 12V',
+    'V12 corresponde ao formato do e quantidade cilindros do motor, já 12v, revela a quantidade de válvulas no motor "12 válvulas?"'
+)
+criaCartao(
+    'Conhcimentos gerais',
+    'Quantos prêmios do Óscar foram atribuídos em 2024?',
+    '23'
 )
 
+criaCartao(
+    'Matemática',
+    'Raiz de 144',
+    '12'
+)
+
+criaCartao(
+    'Mecânica',
+    'Qual a peça que acompanha o pistão?',
+    'Biela"'
+)
